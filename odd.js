975433e8fcd33b5f1824c3db55db16aa618cab51
Jessica <jessica.leung18@gmail.com>
2022-02-05 19:16:33 -0500

    Odd Checker Assignment

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = function(num) {
+  return num % 2 === 1
+}
+
+const elevenIsOdd = isOdd(11);
+const tenIsOdd = isOdd(10);
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+
